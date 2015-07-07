2a0118ccdca6ad746eedd3a3249c8fbef24b73d1
Jorge Bejar <jorge@wyeworks.com>
2015-07-06 21:33:19 -0300

    Response when error should be formatted properly in Rails API if local request

--- actionpack/test/dispatch/routing_test.rb
+++ actionpack/test/dispatch/routing_test.rb
@@ -3270,6 +3270,7 @@ class TestRoutingMapper < ActionDispatch::IntegrationTest
     expected_params = {
       controller: 'downloads',
       action:     'show',
+      format:     'tar',
       id:         '1'
     }
 
